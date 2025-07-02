db293313f11ceb327036f5828f4e20e93e6c987e
csolatorio <46466477+csolatorio@users.noreply.github.com>
2025-07-02 09:26:03 +0800

    Update navbar.tsx

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -29,7 +29,7 @@ const Navbar: React.FC = () => {
     <nav className="bg-[#001A29] text-white px-6 py-4 relative z-50 shadow-md ">
       <div className="flex items-center justify-between md:gap-6 px-6 max-w-7xl mx-auto">
         {/* Logo */}
-        <Link to="/" className="flex items-center">
+        <Link to="/about-me" className="flex items-center">
           <img src={Logo} alt="Logo" className="h-15 w-auto object-contain" />
         </Link>
 
